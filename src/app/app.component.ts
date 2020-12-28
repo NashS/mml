be72bb765837c8056fbb368d9ef4596e893e8915
NashS <nasseq@gmail.com>
2020-12-27 19:17:43 -0500

    close sidenav when changing to gt-xs viewport

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { isPlatformBrowser , DOCUMENT} from '@angular/common';
 import { environment } from '../environments/environment';
 import { Component , OnInit, Inject, PLATFORM_ID} from '@angular/core';
+import { MediaChange, MediaObserver } from '@angular/flex-layout';
 
 @Component({
   selector: 'app-root',
@@ -8,17 +9,27 @@ import { Component , OnInit, Inject, PLATFORM_ID} from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  title = 'mml';
 
-    public ngOnInit(): void {
-        if (!isPlatformBrowser(this.platformId)) {
-            const bases = this.document.getElementsByTagName('base');
-            if (bases.length > 0) {
-                bases[0].setAttribute('href', environment.baseHref);
-            }
+  public opened: boolean;
+
+  ngOnInit(): void {
+      if (!isPlatformBrowser(this.platformId)) {
+          const bases = this.document.getElementsByTagName('base');
+          if (bases.length > 0) {
+              bases[0].setAttribute('href', environment.baseHref);
+          }
+      }
+      this.mediaObserver.asObservable()
+      .subscribe((changes: MediaChange[]) => {
+        if(changes.some(change => change.mqAlias === 'gt-xs')) {
+          this.opened = false;
         }
-    }
+      });
 
+  }
 
- constructor(@Inject(PLATFORM_ID) private platformId: any, @Inject(DOCUMENT) private document: any) {}
+  constructor(@Inject(PLATFORM_ID) private platformId: any,
+              @Inject(DOCUMENT) private document: any,
+              private mediaObserver: MediaObserver) {}
+
 }
