0a1b4c9374cbd4e34330e7bdc74659e1e6dc693b
NashS <nasseq@gmail.com>
2021-02-03 20:00:57 -0500

    trying to get serverless + universal working with angular 9

--- src/app/app.server.module.ts
+++ src/app/app.server.module.ts
@@ -1,25 +1,14 @@
-import { AppComponent } from './app.component';
+import { NgModule } from '@angular/core';
+import { ServerModule } from '@angular/platform-server';
+
 import { AppModule } from './app.module';
-import {NgModule} from '@angular/core';
-import {ServerModule, ServerTransferStateModule} from '@angular/platform-server';
-import {ModuleMapLoaderModule} from '@nguniversal/module-map-ngfactory-loader';
-import { BrowserModule } from '@angular/platform-browser';
-import { NoopAnimationsModule } from '@angular/platform-browser/animations';
-import {FlexLayoutServerModule} from '@angular/flex-layout/server';
+import { AppComponent } from './app.component';
 
-    @NgModule({
- bootstrap: [AppComponent],
-
-        imports:[
- BrowserModule.withServerTransition({appId: 'app-root'}),
-
- AppModule,
-
-            ServerModule,
-            NoopAnimationsModule,
-            ModuleMapLoaderModule,
-            ServerTransferStateModule, // comment
-            FlexLayoutServerModule
-        ]
-    })
-    export class AppServerModule {}
+@NgModule({
+  imports: [
+    AppModule,
+    ServerModule,
+  ],
+  bootstrap: [AppComponent],
+})
+export class AppServerModule {}
